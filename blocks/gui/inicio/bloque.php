8bcd204241764000a37b43139aa3ab95b06a229b
mafeave <mari_fer_0209@hotmail.com>
2016-03-17 16:41:28 -0500

    para el error al cargar el bloque

--- blocks/gui/inicio/bloque.php
+++ blocks/gui/inicio/bloque.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace gui\accesoIncorrecto;
+namespace gui\inicio;
 
 // Evitar un acceso directo a este archivo
 if (! isset ( $GLOBALS ["autorizado"] )) {
@@ -31,64 +31,66 @@ include_once ("Lenguaje.class.php");
 
 // Para evitar redefiniciones de clases el nombre de la clase del archivo bloque debe corresponder al nombre del bloque
 // precedida por la palabra Bloque
-class Bloque implements \Bloque {
-	var $nombreBloque;
-	var $miFuncion;
-	var $miSql;
-	var $miConfigurador;
-	public 
-
-	function __construct($esteBloque, $lenguaje = "") {
-		
-		// El objeto de la clase Configurador debe ser único en toda la aplicación
-		$this->miConfigurador = \Configurador::singleton ();
-		
-		$ruta = $this->miConfigurador->getVariableConfiguracion ( "raizDocumento" );
-		$rutaURL = $this->miConfigurador->getVariableConfiguracion ( "host" ) . $this->miConfigurador->getVariableConfiguracion ( "site" );
-		
-		if (! isset ( $esteBloque ["grupo"] ) || $esteBloque ["grupo"] == "") {
-			$ruta .= "/blocks/" . $esteBloque ["nombre"] . "/";
-			$rutaURL .= "/blocks/" . $esteBloque ["nombre"] . "/";
-		} else {
-			$ruta .= "/blocks/" . $esteBloque ["grupo"] . "/" . $esteBloque ["nombre"] . "/";
-			$rutaURL .= "/blocks/" . $esteBloque ["grupo"] . "/" . $esteBloque ["nombre"] . "/";
-		}
-		
-		$this->miConfigurador->setVariableConfiguracion ( "rutaBloque", $ruta );
-		$this->miConfigurador->setVariableConfiguracion ( "rutaUrlBloque", $rutaURL );
-		
-		$this->miFuncion = new Funcion ();
-		$this->miSql = new Sql ();
-		$this->miFrontera = new Frontera ();
-		$this->miLenguaje = new Lenguaje ();
-	}
-	public function bloque() {
-		if (isset ( $_REQUEST ['botonCancelar'] ) && $_REQUEST ['botonCancelar'] == "true") {
-			$this->miFuncion->redireccionar ( "paginaPrincipal" );
-		} else {
-			
-			$this->miFrontera->setSql ( $this->miSql );
-			$this->miFrontera->setFuncion ( $this->miFuncion );
-			$this->miFrontera->setLenguaje ( $this->miLenguaje );
-			
-			$this->miFuncion->setSql ( $this->miSql );
-			$this->miFuncion->setLenguaje ( $this->miLenguaje );
-			
-			if (! isset ( $_REQUEST ['action'] )) {
+if (! class_exists ( '\\gui\\inicio\\Bloque' )) {
+	if (class_exists ( 'Bloque' ) === false) {
+		class Bloque implements \Bloque {			
+			var $nombreBloque;
+			var $miFuncion;
+			var $miSql;
+			var $miConfigurador;		
+			public function __construct($esteBloque, $lenguaje = "") {
 				
-				$this->miFrontera->frontera ();
-			} else {
+				// El objeto de la clase Configurador debe ser único en toda la aplicación
+				$this->miConfigurador = \Configurador::singleton ();
 				
-				$respuesta = $this->miFuncion->action ();
+				$ruta = $this->miConfigurador->getVariableConfiguracion ( "raizDocumento" );
+				$rutaURL = $this->miConfigurador->getVariableConfiguracion ( "host" ) . $this->miConfigurador->getVariableConfiguracion ( "site" );
 				
-				// Si $respuesta==false, entonces se debe recargar el formulario y mostrar un mensaje de error.
-				if (! $respuesta) {
-					
-					$miBloque = $this->miConfigurador->getVariableConfiguracion ( 'esteBloque' );
-					$this->miConfigurador->setVariableConfiguracion ( 'errorFormulario', $miBloque ['nombre'] );
+				if (! isset ( $esteBloque ["grupo"] ) || $esteBloque ["grupo"] == "") {
+					$ruta .= "/blocks/" . $esteBloque ["nombre"] . "/";
+					$rutaURL .= "/blocks/" . $esteBloque ["nombre"] . "/";
+				} else {
+					$ruta .= "/blocks/" . $esteBloque ["grupo"] . "/" . $esteBloque ["nombre"] . "/";
+					$rutaURL .= "/blocks/" . $esteBloque ["grupo"] . "/" . $esteBloque ["nombre"] . "/";
 				}
-				if (! isset ( $_REQUEST ['procesarAjax'] )) {
-					$this->miFrontera->frontera ();
+				
+				$this->miConfigurador->setVariableConfiguracion ( "rutaBloque", $ruta );
+				$this->miConfigurador->setVariableConfiguracion ( "rutaUrlBloque", $rutaURL );
+				
+				$this->miFuncion = new Funcion ();
+				$this->miSql = new Sql ();
+				$this->miFrontera = new Frontera ();
+				$this->miLenguaje = new Lenguaje ();
+			}
+			public function bloque() {
+				if (isset ( $_REQUEST ['botonCancelar'] ) && $_REQUEST ['botonCancelar'] == "true") {
+					$this->miFuncion->redireccionar ( "paginaPrincipal" );
+				} else {
+					
+					$this->miFrontera->setSql ( $this->miSql );
+					$this->miFrontera->setFuncion ( $this->miFuncion );
+					$this->miFrontera->setLenguaje ( $this->miLenguaje );
+					
+					$this->miFuncion->setSql ( $this->miSql );
+					$this->miFuncion->setLenguaje ( $this->miLenguaje );
+					
+					if (! isset ( $_REQUEST ['action'] )) {
+						
+						$this->miFrontera->frontera ();
+					} else {
+						
+						$respuesta = $this->miFuncion->action ();
+						
+						// Si $respuesta==false, entonces se debe recargar el formulario y mostrar un mensaje de error.
+						if (! $respuesta) {
+							
+							$miBloque = $this->miConfigurador->getVariableConfiguracion ( 'esteBloque' );
+							$this->miConfigurador->setVariableConfiguracion ( 'errorFormulario', $miBloque ['nombre'] );
+						}
+						if (! isset ( $_REQUEST ['procesarAjax'] )) {
+							$this->miFrontera->frontera ();
+						}
+					}
 				}
 			}
 		}
